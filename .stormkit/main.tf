3147014464db9c82e9c0ee350d6ba0c3ef2bbc88
razetka <128233067+razetka2010@users.noreply.github.com>
2026-01-16 22:44:34 +0500

    Update main.tf

--- .stormkit/main.tf
+++ .stormkit/main.tf
@@ -1,1 +1,24 @@
 
+terraform {
+  required_providers {
+    stormkit = {
+      source = "stormkit-io/stormkit"
+    }
+  }
+}
+
+provider "stormkit" {
+  # Токен будет установлен через переменные окружения
+}
+
+resource "stormkit_application" "gdz_bot" {
+  name        = "gdz-navigator-bot"
+  description = "Telegram bot for GDZ 7-9 grades"
+  region      = "europe-west1"
+  
+  environment_variables = {
+    BOT_TOKEN   = var.bot_token
+    WEB_APP_URL = "https://razetka2010.github.io/gdz-navigator/"
+    PORT        = "8080"
+  }
+}
